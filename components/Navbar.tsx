87109a7aa5730122a78294168ae8e2faee19c83a
chiranaportfolio <oshadichirana@gmail.com>
2025-02-19 15:00:36 +0530

    Update Navbar.tsx

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -48,7 +48,7 @@ const Navbar = () => {
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex items-center justify-between h-16">
         <Link href="#hero" className="flex items-center">
-         <img src="/logo.png" alt="Chirana Logo" className="h-16 w-auto max-w-[150px]" />
+         <img src="/portfolio/logo.png" alt="Chirana Logo" className="h-16 w-auto max-w-[150px]" />
           </Link>
 
 
